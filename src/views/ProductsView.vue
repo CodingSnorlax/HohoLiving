e4666b9b1b0086d9c323a3c8c8e996d3a7fe8dcc
Karen Huang <thinknfix2021@gmail.com>
2022-03-28 19:43:59 +0800

    tempCategory 暫存問題

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -3,34 +3,38 @@
     <!-- 產品類別選單 -->
     <div class="row">
       <div class="col-12">
-        <nav
+        <ul
+          id="categoryList"
           class="w-100 pt-10 pe-10 me-20 list-unstyled sticky-top product-category-list text-secondary"
         >
           <h3 class="fs-3 pe-4 mb-4 product-category-list-item">產品類別</h3>
           <ul class="list-unstyled">
             <li
               class="d-block fs-5 pe-4 ps-2 mb-3 py-1 text-decoration-none product-category-list-item"
-              @click="getProductData()"
+              @click="getProductData(), (tempCategory = 'all')"
             >
-              <router-link class="" to="/products">全部產品</router-link>
+              <router-link
+                :class="{ active: itemCategory === 'all' }"
+                >全部產品</router-link
+              >
             </li>
             <template v-for="item in category" :key="item">
               <li
                 class="d-block fs-5 pe-4 ps-2 mb-3 py-1 text-decoration-none product-category-list-item"
-                @click="getProductData(page, item)"
+                @click="getProductData(page, item), (tempCategory = item)"
               >
                 <router-link
                   class="text-decoration-none"
-                  :to="{ query: { category: item } }"
+                  :class="{ active: tempCategory === item }"
                   >{{ item }}</router-link
                 >
               </li>
             </template>
           </ul>
-        </nav>
+        </ul>
       </div>
     </div>
-    <!-- 主要商品列表區 -->
+    <!-- 主要商品列表區 (卡片) -->
     <div class="row">
       <h2 class="text-center text-secondary pt-12 pb-16">好好精選</h2>
       <!-- 卡片 -->
@@ -93,19 +97,19 @@ export default {
       category: [],
       // 自己寫的 BS component
       pagination: {},
-      // 狀態樣式
-      isActive: ''
+      // 當下篩選類別，控制狀態樣式
+      tempCategory: 'all'
     }
   },
   components: {
     ProductPagination
   },
   // 嘗試 router-link 寫法
-  // watch: {
-  //   $route (to) {
-  //     console.log(to.query.category)
-  //   }
-  // },
+  watch: {
+    tempCategory () {
+      this.getProductData()
+    }
+  },
   methods: {
     getProductData (page = 1, query) {
       let url = `${process.env.VUE_APP_API}/v2/api/${process.env.VUE_APP_PATH}/products?page=${page}`
@@ -113,7 +117,6 @@ export default {
         url = `${process.env.VUE_APP_API}/v2/api/${process.env.VUE_APP_PATH}/products?category=${query}`
       }
       this.$http.get(url).then((res) => {
-        console.log(res.data)
         this.products = res.data.products
         this.pagination = res.data.pagination
       })
@@ -158,7 +161,7 @@ export default {
       cursor: pointer;
       text-decoration: none;
     }
-    a.router-link-active{
+    a.active {
       border-left: 3px solid gray;
     }
     border-left: 3px solid transparent;
